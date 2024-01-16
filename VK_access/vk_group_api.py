b4cd77a948177e13f2fe48d1109daa47774e687a
eugiv <eugene.ivanov@ymail.com>
2024-01-16 23:53:14 +0300

    users contact and photos data

--- VK_access/vk_group_api.py
+++ VK_access/vk_group_api.py
@@ -1,4 +1,6 @@
 import vk_api
+from random import random
+from itertools import chain
 from pprint import pprint
 
 
@@ -9,13 +11,49 @@ class VKBotAPI:
         self.age = age
         self.sex = sex
         self.user_init_link = "https://vk.com/id"
+        self.vk_session = vk_api.VkApi(token=self.api_token)
 
-    def api_initiate(self):
-        _session = vk_api.VkApi(token=self.api_token)
-        return _session
+    @staticmethod
+    def likes_finder(user_pics) -> list:
+        likes_cnt = []
+        for pic in chain.from_iterable([user_pics["items"]]):
+            max_likes = str(pic["likes"]["count"])
+            likes_cnt.append(int(max_likes))
+        likes_cnt = sorted(likes_cnt)[-3:]
+
+        return likes_cnt
+
+    def get_user_pics(self, user_id):
+        user_pics = self.vk_session.method(
+            "photos.getAll", {"owner_id": user_id, "extended": True}
+        )
+
+        pics_ids = []
+        for likes in user_pics["items"]:
+            pics_selection = [
+                x
+                for x in [likes["likes"]["count"]]
+                if x in self.likes_finder(user_pics)
+            ]
+            if pics_selection:
+                # max_pics_size = max(likes["sizes"], key=lambda x: x.get("height", 0) + x.get("width", 0))
+                pics_ids.append(likes["id"])
+
+        return pics_ids
+
+    def make_attachment(self, media_id=456239116, user_id=5469708):
+        # for media_id in media_ids:
+        self.vk_session.method(
+            "messages.send",
+            {
+                "user_id": user_id,
+                "random_id": random(),
+                "attachment": f"photo{user_id}_{media_id}",
+            },
+        )
 
     def get_user_info(self):
-        user_search_res = self.api_initiate().method(
+        user_search_res = self.vk_session.method(
             "users.search",
             {
                 "hometown": self.hometown,
@@ -25,18 +63,17 @@ class VKBotAPI:
             },
         )
 
-        users_links = []
+        users_main_data = []
         for usr_lnk in user_search_res["items"]:
-            users_links.append(self.user_init_link + str(usr_lnk["id"]))
-
-        user_pics = self.api_initiate().method(
-            "photos.getAll", {"owner_id": 831751992, "extended": True}
-        )
-        cnt = []
-        for pic in user_pics["items"]:
-            max_likes = str(pic["likes"]["count"])
-            cnt.append(int(max_likes))
-        cnt = sorted(cnt)[-3:]
-        pprint(cnt)
-
-        return user_search_res
+            get_pics_ids = self.get_user_pics(
+                usr_lnk["id"]
+            )  # implement below in self.make_attachment()
+            user_data = [
+                usr_lnk["first_name"],
+                usr_lnk["last_name"],
+                self.user_init_link + str(usr_lnk["id"]),
+                self.make_attachment(),  # here
+            ]
+            users_main_data.append(user_data)
+        pprint(users_main_data)
+        return users_main_data
