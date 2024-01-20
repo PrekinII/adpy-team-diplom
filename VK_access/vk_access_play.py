d6801ed2a5d062c31b1dbeb70215174d6ec2ee71
eugiv <eugene.ivanov@ymail.com>
2024-01-20 13:14:17 +0300

    modified db structure

--- VK_access/vk_access_play.py
+++ VK_access/vk_access_play.py
@@ -3,5 +3,7 @@ from VK_access.vk_group_api import VKBotAPI
 
 # 313568452
 if __name__ == "__main__":
-    vk_inst = VKBotAPI(vk_token, "Щелково", 0, 1)  # make exception handler if user profile is private
+    vk_inst = VKBotAPI(
+        vk_token, "Щелково", 0, 1
+    )  # make exception handler if user profile is private
     vk_inst.process_user_pics(5469708)
